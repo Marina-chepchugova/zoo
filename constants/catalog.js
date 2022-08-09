0ed5385662dd4205a35cc8935d605a21ca3efb45
Marina <98214485+Marina-chepchugova@users.noreply.github.com>
2022-08-10 01:46:27 +1100

    Components

---
+++ constants/catalog.js
@@ -0,0 +1,73 @@
+const CATALOG = [
+{
+	id:'el1',
+	name: 'Метис  ',
+	img: 'https://i.ibb.co/3CDDt8L/b2ed5813d69c21334932da1f479a7a7e.jpg ',
+	price: 2000,
+
+},
+{
+	id:'el2',
+	name: 'Французский бульдог',
+	img: 'https://i.ibb.co/SP3LjT6/5273a8b632df5cb9da198e7874a00453.jpg ',
+	price: 2000,
+
+},
+{
+	id: 'el3',
+	name: ' Корги ',
+	img: 'https://i.ibb.co/7XFs4k2/ba11d6579281637d8323c5ecaf469073.jpg',
+	price: 2000,
+
+},
+{
+	id: 'el4',
+	name: ' Корги ',
+	img: 'https://i.ibb.co/7XFs4k2/ba11d6579281637d8323c5ecaf469073.jpg',
+	price: 2000,
+
+},
+{
+	id: 'el5',
+	name: ' Корги ',
+	img: 'https://i.ibb.co/7XFs4k2/ba11d6579281637d8323c5ecaf469073.jpg',
+	price: 2000,
+
+},
+{
+	id: 'el6',
+	name: ' Корги ',
+	img: 'https://i.ibb.co/7XFs4k2/ba11d6579281637d8323c5ecaf469073.jpg',
+	price: 2000,
+
+},
+{
+	id: 'el7',
+	name: ' Корги ',
+	img: 'https://i.ibb.co/7XFs4k2/ba11d6579281637d8323c5ecaf469073.jpg',
+	price: 2000,
+
+},
+{
+	id: 'el8',
+	name: ' Корги ',
+	img: 'https://i.ibb.co/7XFs4k2/ba11d6579281637d8323c5ecaf469073.jpg',
+	price: 2000,
+
+},
+{
+	id: 'el9',
+	name: 'Метис  ',
+	img: 'https://i.ibb.co/3CDDt8L/b2ed5813d69c21334932da1f479a7a7e.jpg ',
+	price: 2000,
+
+},
+{
+	id: 'el10',
+	name: 'Метис  ',
+	img: 'https://i.ibb.co/3CDDt8L/b2ed5813d69c21334932da1f479a7a7e.jpg ',
+	price: 2000,
+
+}
+
+];
