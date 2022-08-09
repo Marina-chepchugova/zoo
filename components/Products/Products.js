82896fb9e77a52432442338d34850bc998edd1ce
Marina <98214485+Marina-chepchugova@users.noreply.github.com>
2022-08-10 08:21:16 +1100

    correction

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -7,7 +7,7 @@ class Products{
 			htmlCatalog+=`
 			<li class="products-element">
 			<span class="products-element__name">${name}</span>
-			<img class="products-element__img  src="${img}"/>
+			<img class="products-element__img"  src="${img}"/>
 			<span class="products-element__price">${price}</span>
 			<button class="products-element__btn">Добавить в корзину</button>
 			</li>
